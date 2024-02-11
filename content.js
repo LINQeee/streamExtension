ab466e527d225ee4a2786f663376697568ce79fb
Adrian Vvedenskii <adrianvved@gmail.com>
2024-02-11 22:51:25 +0300

    marking messages while pressing

--- content.js
+++ content.js
@@ -1,10 +1,18 @@
 let messages = document.querySelectorAll("yt-live-chat-text-message-renderer");
+let mouseDown = false;
+
+document.addEventListener("mousedown", () => (mouseDown = true));
+document.addEventListener("mouseup", () => (mouseDown = false));
 
 messages.forEach((message) => {
-  message.addEventListener("dblclick", (event) => {
-    event.target.closest(
-      "yt-live-chat-text-message-renderer"
-    ).style.backgroundColor = "red";
-    console.log("clicked");
+  message.addEventListener("mouseenter", (event) => {
+    if (!mouseDown) return;
+    markMessage(event.target);
   });
+
+  message.addEventListener("dblclick", (event) => markMessage(event.target));
 });
+
+const markMessage = (message) =>
+  (message.closest("yt-live-chat-text-message-renderer").style.backgroundColor =
+    "black");
