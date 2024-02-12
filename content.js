66e3b01cda7b7fa6e4f2b087a6af0524e17771c1
Adrian Vvedenskii <adrianvved@gmail.com>
2024-02-12 07:26:24 +0300

    all chat messages fix

--- content.js
+++ content.js
@@ -1,18 +1,29 @@
-let messages = document.querySelectorAll("yt-live-chat-text-message-renderer");
 let mouseDown = false;
+let markedMessages = [];
 
-document.addEventListener("mousedown", () => (mouseDown = true));
-document.addEventListener("mouseup", () => (mouseDown = false));
+const updateMessages = () => {
+  let messages = document.querySelectorAll(
+    "yt-live-chat-text-message-renderer"
+  );
+  document.addEventListener("mousedown", () => (mouseDown = true));
+  document.addEventListener("mouseup", () => (mouseDown = false));
 
-messages.forEach((message) => {
-  message.addEventListener("mouseenter", (event) => {
-    if (!mouseDown) return;
-    markMessage(event.target);
+  messages.forEach((message) => {
+    if (markedMessages.some((id) => id === message.getAttribute("id")))
+      markMessage(message);
+    message.addEventListener("mouseenter", (event) => {
+      if (!mouseDown) return;
+      markMessage(event.target);
+    });
+
+    message.addEventListener("dblclick", (event) => markMessage(event.target));
   });
+};
 
-  message.addEventListener("dblclick", (event) => markMessage(event.target));
-});
+const markMessage = (message) => {
+  message.closest("yt-live-chat-text-message-renderer").style.backgroundColor =
+    "black";
+  markedMessages.push(message.getAttribute("id"));
+};
 
-const markMessage = (message) =>
-  (message.closest("yt-live-chat-text-message-renderer").style.backgroundColor =
-    "black");
+setInterval(updateMessages, 100);
